cba3c29e3e8c652d77c630750d73ace129a9d96a
Lata Talreja <ltalreja177@gmail.com>
2024-09-11 03:09:45 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict";//treat all JS code as newer version 
+
+
+//alert(3+5)//we are using nodejs, not browser
+
+console.log(3+4)
+console.log("read")   //code readibility should be high
+
+
+let name="lata"
+let age=5
+let isLoggedIn= false
+let state;
+
+//number= 2 to power 53 it provide space in memory
+//bigint   .... this is usedin trading websites ...reddit etc they huge no. of space
+//string....."",  ''
+//boolean ....true or false
+//null .....stand alone value in js as well as data type in js....empty representation ..." "this is not preferd as a empty.....null refferd to having empty vlue
+state=null;
+//undefined ......value does not defined....var define value does not given
+//symbol....manily used in reacts to find unique components or find uniqueness of components 
+
+
+//undefined.....it is type  
+//null..... it is object
+
+
+
+
+//object
+
+console.log(typeof"lata")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
