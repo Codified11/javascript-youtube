6b2c8e228ad5fde67d5e3bd31f1a82bd98a39244
Lata Talreja <ltalreja177@gmail.com>
2024-09-19 02:56:13 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,55 @@
+// let score = 33     .....number
+// let score = "33"       .....string    ....33
+// let score = "33abc"       ....Nan ....not a number
+
+// let score = null          .....0
+// let score =undefined        .....NaN
+
+// let score = "hitesh"    .....Nan
+
+// let score =true; .....1
+// let score =false  ......0
+
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+
+// let valueInNumber= Number(score)
+// console,console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+//"33" = 33
+// "33abc"=NaN
+//true = 1 ;false= 0;
+
+
+// let isLoggedIn =1            //we want  in boolean
+
+// let isLoggedIn =0      //false
+ //let isLoggedIn =""    ....false
+
+ //let isLoggedIn ="hitesh"   ...true
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+
+//"" =false
+// "hitesh"=true
+//0 =false; 1= true
+
+
+
+let someNumber = 32;
+let stringNumber= String(someNumber)
+console.log(typeof (stringNumber));
+console.log(stringNumber);
+
+
