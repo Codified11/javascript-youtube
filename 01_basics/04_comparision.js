a5eaf63f0eee74d6fa4c893e2d45247f4eee4d22
Lata Talreja <ltalreja177@gmail.com>
2024-09-27 02:55:17 +0000

    container config done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// when comparision between different datatype......
+console.log("2">1);
+console.log("02">1);
+// this give unpredictable answer sometimes.... so better to use same datatype for comparision
+
+console.log(null >0); //false
+console.log(null>=0);//true
+console.log(null==0); 
+
+
+
+// give false.....
+
+// console.log(undefined ==0);
+// console.log(undefined>0);
+// console.log(undefined>=0);
+
+// strict check...(===)
+// this not only check value it checks datatype also....
+
+console.log("2"==2);   //it give true
+console.log("2"===2);   //it give problem dur to strict check //false
+
+
+
+
